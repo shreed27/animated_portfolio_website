fd3cfdfb888499f7a9e25c51cd21e4ccc47d0358
Shreed Shrivastava <136893148+shreed27@users.noreply.github.com>
2025-03-25 16:20:23 +0530

    Update Resume.jsx

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -76,18 +76,13 @@ const Resume = () => {
                   B.Tech, Computer Science
                 </h1>
                 <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
-                  College Name
+                  CSVTU, SSIPMT
                 </span>
                 <span className=" text-[.9rem] font-semibold text-yellow-500 sm:text-base">
-                  Year 2023 - Year 2027
+                  SEP 2023 - JAN 2027
                 </span>
                 <p className=" text-[.9rem] text-justify text-gray-500">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Excepturi nihil aut quae obcaecati alias placeat totam aperiam
-                  pariatur quam? Accusantium minima aperiam error obcaecati
-                  autem doloribus deserunt qui, laborum incidunt amet, numquam
-                  eos cum tempora. Debitis, esse quae corporis deserunt officia
-                  quos in a, quaerat vero odio vitae.
+                
                 </p>
               </div>
             </div>
