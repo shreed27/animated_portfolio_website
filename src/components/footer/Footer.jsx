7cb5090fad40a09605f062ec3b65ca4be4e9d6b7
Shreed Shrivastava <136893148+shreed27@users.noreply.github.com>
2025-03-25 16:25:19 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
           </a>
         </div>
         <div className="sm:text-[12px]">
-          | Copyright &copy; <span>2023 Dev AlphaSpace </span> All rights reserved
+          | Copyright &copy; <span>2025 Shreed Shrivastava </span> All rights reserved
           <a href="#"></a> |
         </div>
       </div>
